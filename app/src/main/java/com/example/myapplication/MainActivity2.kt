f19effc322a9d13c3aa85b9b47a8145f131793e6
Pavel Balitskii <79817751+alest747@users.noreply.github.com>
2022-09-30 23:20:53 +0800

    Update MainActivity2.kt

--- app/src/main/java/com/example/myapplication/MainActivity2.kt
+++ app/src/main/java/com/example/myapplication/MainActivity2.kt
@@ -9,15 +9,15 @@ import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity2 : AppCompatActivity() {
-private lateinit var tvInfo:TextView
+private lateinit var tvInfo:TextView  //приватная переменная с задержкой нашего TextView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
-        val message = intent.getStringExtra("key")
-        tvInfo = findViewById<TextView?>(R.id.tvText)
-        tvInfo.text = "Поздравляем!!!\n$message"
+        val message = intent.getStringExtra("key")  //создаем переменную и помещаем в нее информацию с предыдущего активити, key это ключ для получения информации
+        tvInfo = findViewById(R.id.tvText)  //находит наш TextView по id
+        tvInfo.text = "Поздравляем!!!\n $message"  //помещаем в TextView полученую информацию message
     }
 }
